782be7fcb265f3090ca1253117e460733d650dea
陈高建 <1783968112@qq.com>
2024-07-04 17:30:15 +0800

    Adding Search and Pagination

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -109,31 +109,36 @@ export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
 ) {
+  await new Promise((resolve) => setTimeout(resolve, 200));
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
-    const invoices = await sql<InvoicesTable>`
-      SELECT
-        invoices.id,
-        invoices.amount,
-        invoices.date,
-        invoices.status,
-        customers.name,
-        customers.email,
-        customers.image_url
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      WHERE
-        customers.name ILIKE ${`%${query}%`} OR
-        customers.email ILIKE ${`%${query}%`} OR
-        invoices.amount::text ILIKE ${`%${query}%`} OR
-        invoices.date::text ILIKE ${`%${query}%`} OR
-        invoices.status ILIKE ${`%${query}%`}
-      ORDER BY invoices.date DESC
-      LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
-    `;
-
-    return invoices.rows;
+    const invoices = await prisma.invoice.findMany({
+      where: {
+        OR: [
+          // { amount: { contains: query } },
+          // { date: { contains: query } },
+          { status: { contains: query } },
+          {
+            customer: {
+              OR: [
+                { name: { contains: query } },
+                { email: { contains: query } },
+              ],
+            },
+          },
+        ],
+      },
+      include: {
+        customer: true,
+      },
+      orderBy: {
+        date: 'desc',
+      },
+      take: ITEMS_PER_PAGE,
+      skip: offset,
+    });
+    return invoices;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch invoices.');
@@ -142,18 +147,25 @@ export async function fetchFilteredInvoices(
 
 export async function fetchInvoicesPages(query: string) {
   try {
-    const count = await sql`SELECT COUNT(*)
-    FROM invoices
-    JOIN customers ON invoices.customer_id = customers.id
-    WHERE
-      customers.name ILIKE ${`%${query}%`} OR
-      customers.email ILIKE ${`%${query}%`} OR
-      invoices.amount::text ILIKE ${`%${query}%`} OR
-      invoices.date::text ILIKE ${`%${query}%`} OR
-      invoices.status ILIKE ${`%${query}%`}
-  `;
+    const count = await prisma.invoice.count({
+      where: {
+        OR: [
+          // { amount: { contains: query } },
+          // { date: { contains: query } },
+          { status: { contains: query } },
+          {
+            customer: {
+              OR: [
+                { name: { contains: query } },
+                { email: { contains: query } },
+              ],
+            },
+          },
+        ],
+      }
+    });
 
-    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+    const totalPages = Math.ceil(Number(count) / ITEMS_PER_PAGE);
     return totalPages;
   } catch (error) {
     console.error('Database Error:', error);
