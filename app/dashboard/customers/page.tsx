1606194466f9c7fb2036c6b211cae75ee5f42211
陈高建 <1783968112@qq.com>
2024-07-05 17:13:46 +0800

    Adding Metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,9 @@
 import {auth} from "@/auth";
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+    title: 'Customers | Acme Dashboard',
+};
 export default async function Page() {
     const session = await auth()
     return <p>Customers Page {session?.user?.name} {session?.user?.email}</p>;
